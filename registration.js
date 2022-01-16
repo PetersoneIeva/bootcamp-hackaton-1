ae7e5927f48040fe7614f98bd1617401f6c55186
Santa <84859451+s-avo@users.noreply.github.com>
2022-01-16 19:44:14 +0200

    update

--- registration.js
+++ registration.js
@@ -1,12 +1,22 @@
-const register = document.getElementById('register-btn');
+// very limited functionality
+// when the button is clicked, a message appears under button with the name of the person,
+// all input fields are cleared, and button disabled
+
+const register = document.getElementById('register-btn')
 const confirmation = document.getElementById('confirmation');
 const name = document.getElementById('name');
+const inputFields = document.querySelectorAll('.form-control');
 
 register.addEventListener('click', () => {
-    confirmation.innerHTML += `Thank you for registering, ${name.value}! We will contact you shortly.`
-    disableButton();
-  });
+  confirmation.innerHTML += `Thank you for registering, ${name.value}! We will contact you shortly.`
+      for (let i = 0; i < inputFields.length; i++) {
+        inputFields[i].value = ''
+  }
 
-  const disableButton = () => {
+  disableButton();
+});
+
+const disableButton = () => {
     document.getElementById('register-btn').disabled = true;
  };
+
