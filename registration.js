961487e7f0d01260db2651b31d11010a5df6e016
Santa <84859451+s-avo@users.noreply.github.com>
2022-01-16 12:18:35 +0200

    styling and a bit of js

---
+++ registration.js
@@ -0,0 +1,13 @@
+const register = document.getElementById('register-btn');
+const confirmation = document.getElementById('confirmation');
+const name = document.getElementById('name');
+
+register.addEventListener('click', () => {
+    confirmation.innerHTML += `Thank you for registering, ${name.value}! We will contact you shortly.`
+    disableButton();
+  });
+
+  const disableButton = () => {
+    document.getElementById('register-btn').disabled = true;
+ };
+ 
