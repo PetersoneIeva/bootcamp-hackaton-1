d223dbf0a62a6ba405a42c11d3fa257ed7f9b123
Santa <84859451+s-avo@users.noreply.github.com>
2022-01-17 00:16:50 +0200

    js update

--- src/registration.js
+++ src/registration.js
@@ -1,9 +1,14 @@
 const register = document.getElementById('register-btn');
 const confirmation = document.getElementById('confirmation');
 const name = document.getElementById('name');
+const inputFields = document.querySelectorAll('.form-control');
 
 register.addEventListener('click', () => {
     confirmation.innerHTML += `Thank you for registering, ${name.value}! We will contact you shortly.`
+    for (let i = 0; i < inputFields.length; i++) {
+      inputFields[i].value = ''
+    }
+    
     disableButton();
   });
 
